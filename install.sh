a9825ca5f9fa028737ea16ff79db2b1baed0272c
Dimitri Racordon <dimitri.racordon@gmail.com>
2021-03-20 01:37:47 +0100

    Create an installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+CC=cc
+# LIB_DIR=/usr/local/lib/
+# BIN_DIR=/usr/local/bin/
+LIB_DIR=/tmp/thedoors
+BIN_DIR=/tmp/thedoors
+
+# Exit when any command fails
+set -e
+
+# Compile the runtime library.
+cd Runtime
+make
+cd ..
+
+# Compile cocodoc.
+swift build -c release
+
+# Install the binaries.
+cp Runtime/build/libcocodol_rt.a ${LIB_DIR}
+cp .build/release/cocodoc ${BIN_DIR}
+
+echo "Runtime library installed at ${LIB_DIR}/libcocodol_rt.a"
+echo "Compiler binary installed at ${BIN_DIR}/cocodoc"
